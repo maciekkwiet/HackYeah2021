5c22e34cf326afa52d3add382b4f642c8c1caf95
Marcin Wos <marcin.wos@cdq.com>
2021-12-11 00:41:20 +0100

    Auth logic

--- src/services/routes/Router.tsx
+++ src/services/routes/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from 'App';
 import { Login } from 'features/auth/pages/Login';
+import { Register } from 'features/auth/pages/Register';
 
 export const Router = () => {
   return (
@@ -8,6 +9,7 @@ export const Router = () => {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
