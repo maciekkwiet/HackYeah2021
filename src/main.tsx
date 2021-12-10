aa325def2d704d604f0bd7b7db7e254e2ea89ece
MaciejKwiet <maciej.kwietniewski@pronos.eu>
2021-12-10 22:47:37 +0000

    add theme

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,11 @@ import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './App';
+import { theme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
