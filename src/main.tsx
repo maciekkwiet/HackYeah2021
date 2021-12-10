4f7a84256b377ef21404ebf2190f73e291c2af4e
MaciejKwiet <maciej.kwietniewski@pronos.eu>
2021-12-10 22:22:32 +0000

    add chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
