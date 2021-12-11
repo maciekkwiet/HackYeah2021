a3cefac55833b6ec0172956d21047d2c06f9c5dd
MaciejKwiet <maciej.kwietniewski@pronos.eu>
2021-12-11 16:23:49 +0000

    Empty needs page

--- src/features/inventory/pages/FindNeeds.tsx
+++ src/features/inventory/pages/FindNeeds.tsx
@@ -1,68 +1,9 @@
-import { useState } from 'react';
-import {
-  Box,
-  Button,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  useDisclosure,
-} from '@chakra-ui/react';
-import { Header } from 'components/Header';
-
-import { AddNewItemForm } from '../components/AddNewItemForm';
-import { ItemList } from '../components/ItemList';
-import { useNewInventoryItem } from '../hooks/useNewInventoryItem';
-import { Inventory } from '../typings/inventory';
+import { Box, Heading } from '@chakra-ui/react';
 
 export const FindNeeds = () => {
-  const { addNewItem } = useNewInventoryItem();
-  const [form, setForm] = useState<Inventory>({
-    name: '',
-    description: '',
-    category: '',
-    image: '',
-    quantity: 0,
-    price: 0,
-    weight: 0,
-    expirationDate: '',
-  });
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const handleChange = ({ target: { name, value } }: any) => {
-    setForm({ ...form, [name]: value });
-  };
-
-  const handleSubmit = async () => {
-    await addNewItem(form);
-    onClose();
-  };
-
   return (
     <Box>
-      <Header text="Mój inwentarz" buttonText="Dodaj przedmiot" onButtonClick={onOpen} />
-      <ItemList onButtonClick={onOpen} />
-      <Modal isOpen={isOpen} onClose={onClose} closeOnOverlayClick={false}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Dodaj przedmiot do inwentarza</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <AddNewItemForm {...form} handleChange={handleChange} />
-          </ModalBody>
-          <ModalFooter>
-            <Button variant="ghost" mr={3} onClick={onClose}>
-              Anuluj
-            </Button>
-            <Button onClick={handleSubmit} colorScheme="brand">
-              Dodaj przedmiot
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+      <Heading>Wyszukaj schroniska</Heading>
     </Box>
   );
 };
