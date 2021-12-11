d541852009f38d501908087d13df09fb33564f7e
MaciejKwiet <maciej.kwietniewski@pronos.eu>
2021-12-11 00:43:13 +0000

    Added box

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,11 @@ import './App.css';
 
 import { useState } from 'react';
 import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons';
-import { Button, Flex, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
 import { Step, Steps, useSteps } from 'chakra-ui-steps';
 import { useUser } from 'use-supabase';
 
-const content = (
-  <Flex py={4}>
-    <Text fontSize="xs">Lecimy z nimi</Text>
-  </Flex>
-);
-
-const steps = [
-  { label: 'Typ konta', content },
-  { label: 'Podstawowe informacje', content },
-  { label: 'Potwierdzenie', content },
-];
+const steps = [{ label: 'Typ konta' }, { label: 'Podstawowe informacje' }, { label: 'Potwierdzenie' }];
 
 const App = () => {
   const [count, setCount] = useState(0);
@@ -27,13 +17,13 @@ const App = () => {
 
   return (
     <>
-      <Steps activeStep={activeStep} color="blue">
-        {steps.map((option) => (
-          <Step colorScheme="primary" label={option.label}>
-            {content}
-          </Step>
-        ))}
-      </Steps>
+      <Box border="1px" borderColor="gray.500" m={10}>
+        <Steps activeStep={activeStep} color="blue">
+          {steps.map((option) => (
+            <Step colorScheme="primary" label={option.label} />
+          ))}
+        </Steps>
+      </Box>
 
       <Flex gridGap={10}>
         <Button {...{ nextStep, prevStep }} width={250} size="md" onClick={() => prevStep()}>
