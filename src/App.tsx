d98c8c0438bfa5b0de6627864f9e911f48c4f2bc
MaciejKwiet <maciej.kwietniewski@pronos.eu>
2021-12-11 01:19:27 +0000

    move some code to register

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons';
-import { Box, Button, Flex } from '@chakra-ui/react';
-import { Step, Steps, useSteps } from 'chakra-ui-steps';
+import { useSteps } from 'chakra-ui-steps';
 import { LogoutButton } from 'features/auth/components/LogoutButton';
 import { useUser } from 'use-supabase';
 
@@ -14,27 +12,9 @@ const App = () => {
 
   return (
     <>
-      <Box border="1px" borderColor="gray.500" m={10} p={7}>
-        {/* Powinno być indigo.600 w colorScheme ale nie działa, pewnie coś easy ale nie korzystałem wcześniej z chakry xD */}
-        <Steps activeStep={activeStep} colorScheme="blue">
-          {steps.map((option) => (
-            <Step colorScheme="primary" label={option.label} />
-          ))}
-        </Steps>
-      </Box>
-
       {user?.email}
       <br />
       <LogoutButton />
-
-      <Flex gridGap={10}>
-        <Button {...{ nextStep, prevStep }} width={250} size="md" onClick={() => prevStep()}>
-          <ArrowBackIcon />
-        </Button>
-        <Button {...{ nextStep, prevStep }} width={250} size="md" onClick={() => nextStep()}>
-          <ArrowForwardIcon />
-        </Button>
-      </Flex>
     </>
   );
 };
--- src/features/auth/pages/Register.tsx
+++ src/features/auth/pages/Register.tsx
@@ -1,56 +1,94 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useSupabase } from 'use-supabase';
+import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons';
+import { Box, Button, Flex } from '@chakra-ui/react';
+import { Step, Steps, useSteps } from 'chakra-ui-steps';
+import { useUser } from 'use-supabase';
 
-export const Register = () => {
-  const supabase = useSupabase();
-  const [loading, setLoading] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate();
+const steps = [{ label: 'Typ konta' }, { label: 'Podstawowe informacje' }, { label: 'Potwierdzenie' }];
 
-  const handleRegister = async () => {
-    setLoading(true);
-
-    try {
-      const { error } = await supabase.auth.signUp({ email, password });
-
-      setLoading(false);
-
-      if (error) throw error;
-
-      navigate('/');
-    } catch (error) {
-      alert(error.error_description || error.message);
-    }
-  };
+const Register = () => {
+  const user = useUser();
+  const { nextStep, prevStep, setStep, reset, activeStep } = useSteps({
+    initialStep: 0,
+  });
 
   return (
-    <div>
-      <div>
-        <h1>Supabase + React</h1>
-        <p>Sign up with your email below</p>
-        <div>
-          <input type="email" placeholder="Your email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <input
-            type="password"
-            placeholder="Your password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div>
-          <button
-            onClick={(e) => {
-              e.preventDefault();
-              handleRegister();
-            }}
-            disabled={loading}
-          >
-            {loading ? <span>Loading</span> : <span>Send magic link</span>}
-          </button>
-        </div>
-      </div>
-    </div>
+    <>
+      <Box border="1px" borderColor="gray.500" m={10} p={7}>
+        {/* Powinno być indigo.600 w colorScheme ale nie działa, pewnie coś easy ale nie korzystałem wcześniej z chakry xD */}
+        <Steps activeStep={activeStep} colorScheme="blue">
+          {steps.map((option) => (
+            <Step colorScheme="primary" label={option.label} />
+          ))}
+        </Steps>
+      </Box>
+
+      <Flex gridGap={10}>
+        <Button {...{ nextStep, prevStep }} width={250} size="md" onClick={() => prevStep()}>
+          <ArrowBackIcon />
+        </Button>
+        <Button {...{ nextStep, prevStep }} width={250} size="md" onClick={() => nextStep()}>
+          <ArrowForwardIcon />
+        </Button>
+      </Flex>
+    </>
   );
 };
+
+export default Register;
+
+// import { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { useSupabase } from 'use-supabase';
+
+// export const Register = () => {
+//   const supabase = useSupabase();
+//   const [loading, setLoading] = useState(false);
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const navigate = useNavigate();
+
+//   const handleRegister = async () => {
+//     setLoading(true);
+
+//     try {
+//       const { error } = await supabase.auth.signUp({ email, password });
+
+//       setLoading(false);
+
+//       if (error) throw error;
+
+//       navigate('/');
+//     } catch (error) {
+//       alert(error.error_description || error.message);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <h1>Supabase + React</h1>
+//         <p>Sign up with your email below</p>
+//         <div>
+//           <input type="email" placeholder="Your email" value={email} onChange={(e) => setEmail(e.target.value)} />
+//           <input
+//             type="password"
+//             placeholder="Your password"
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//           />
+//         </div>
+//         <div>
+//           <button
+//             onClick={(e) => {
+//               e.preventDefault();
+//               handleRegister();
+//             }}
+//             disabled={loading}
+//           >
+//             {loading ? <span>Loading</span> : <span>Send magic link</span>}
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
