21db6ae521d92334b89ae3fb3601cacef9dee2a9
Marcin Wos <marcin.wos@cdq.com>
2021-12-11 03:35:41 +0100

    buttons

--- src/features/auth/components/StepButtons.tsx
+++ src/features/auth/components/StepButtons.tsx
@@ -1,5 +1,5 @@
 import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons';
-import { Button, Flex } from '@chakra-ui/react';
+import { Button, Flex, Text } from '@chakra-ui/react';
 
 export type StepButtonsProps = {
   prevStep?: () => void;
@@ -10,13 +10,20 @@ export const StepButtons = ({ prevStep, nextStep }: StepButtonsProps) => {
   return (
     <Flex gridGap={10}>
       {prevStep && (
-        <Button width={250} size="md" onClick={() => prevStep()}>
-          <ArrowBackIcon />
+        <Button
+          colorScheme="teal"
+          variant="outline"
+          width={200}
+          size="md"
+          onClick={() => prevStep()}
+          leftIcon={<ArrowBackIcon />}
+        >
+          <Text fontSize="sm">Poprzedni krok</Text>
         </Button>
       )}
       {nextStep && (
-        <Button width={250} size="md" onClick={() => nextStep()}>
-          <ArrowForwardIcon />
+        <Button width={200} colorScheme="blue" size="md" onClick={() => nextStep()} rightIcon={<ArrowForwardIcon />}>
+          <Text fontSize="sm">Kolejny krok</Text>
         </Button>
       )}
     </Flex>
